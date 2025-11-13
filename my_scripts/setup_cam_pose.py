30bd83cd369fb782dc8646ab83c54ee4c0d5bded
zl3466 <zl3466@nyu.edu>
2025-11-13 01:01:41 -0500

    add pose conversion script

--- my_scripts/setup_cam_pose.py
+++ my_scripts/setup_cam_pose.py
@@ -1,5 +1,8 @@
 import math
 
+camera_height_offset = 0.25
+orbital_radius = 0.75
+
 orbital_angles_degrees = [45, 135, 225, 315]          # 左前、左后、右后、右前
 orbital_view_names     = ["front_left", "back_left", "back_right", "front_right"]
 view_index_counter = 0
@@ -11,5 +14,5 @@ for i, angle_deg in enumerate(orbital_angles_degrees):
     cam_y = lookat_pos[1] + orbital_radius * math.sin(angle_rad) 
     cam_z = lookat_pos[2] + camera_height_offset   
     cam_pos = (cam_x, cam_y, cam_z)
-    cam.set_pose(pos=cam_pos, lookat=look+at_pos)
+    cam.set_pose(pos=cam_pos, lookat=lookat_pos)
     cam.start_recording()
